4b509aa7aebe7b7c9ea03ea9f690aebd718c63b0
jaren <jaren.worme@mail.utoronto.ca>
2024-02-04 00:35:34 -0500

    styled and deployed

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import './App.css';
-// import { ChakraProvider, Container } from '@chakra-ui/react';
 
 
 function App() {
+  // states to keep track of elements
   const [time, setTime] = useState(1500);
   const [sessionInit, setSessionInit] = useState(25);
   const [breakInit, setBreakInit] = useState(5);
@@ -12,11 +12,13 @@ function App() {
   const [isSession, setIsSession] = useState(true);
   const [hasStarted, setHasStarted] = useState(false);
   
+  // temporary/transition elements that don't need state
   var beep = document.getElementById("beep") as HTMLAudioElement;
-
   const minutes = Math.floor(time/60).toString().padStart(2, "0");
   const seconds = Math.floor(time%60).toString().padStart(2, "0");
+  const title = isSession ? "Session" : "Break";
 
+  // useEffect hook keeps track of variables indepently. Needed to auto update times in setInterval function
   useEffect(() => {
     let interval: ReturnType<typeof setInterval>;
     if (sessionRunning){
@@ -44,6 +46,8 @@ function App() {
     return () => clearInterval(interval);
   }, [sessionRunning, breakRunning, sessionInit, time, isSession, breakInit, beep]);
 
+  // handles every increment and decrement time button to set up initial conditions
+  // cannot change these without pressing the reset button after a session has started
   const handleInit = (type: string) => {
     if(!hasStarted){
       if(breakInit<1){
@@ -74,6 +78,7 @@ function App() {
     }
   }
 
+  // handles functionality of the start / stop button
   const handleStartStop = () => {
     if(isSession){
       setSessionRunning(!sessionRunning);
@@ -87,6 +92,7 @@ function App() {
     if(!hasStarted){setHasStarted(true)}
   }
 
+  // resets the clock to its intial state
   const handleReset = () => {
     setTime(1500);
     setBreakInit(5);
@@ -99,24 +105,47 @@ function App() {
     beep.currentTime = 0;
   };
 
-  const title = isSession ? "Session" : "Break";
 
   return (
     <div className="App">
-      
-      <h1 id="break-label">Break Length</h1>
-      <p id="break-length">{ breakInit }</p>
-      <h1 id="session-label">Session Length</h1>
-      <p id="session-length">{ sessionInit }</p>
-      <button id="break-increment" onClick={() => handleInit("bi")}>Break +</button>
-      <button id="break-decrement" onClick={() => handleInit("bd")}>Break -</button>
-      <button id="session-increment" onClick={() => handleInit("si")}>Session +</button>
-      <button id="session-decrement" onClick={() => handleInit("sd")}>Session -</button>
-      <h1 id="timer-label">{title}</h1>
-      <h2 id="time-left">{ minutes }:{ seconds }</h2>
-      <button id="start_stop" onClick={handleStartStop}>{(breakRunning || sessionRunning) ? "Stop" : "Start"}</button>
-      <button id="reset" onClick={handleReset}>Reset</button>
-      
+      <h1>Pomodoro Clock</h1>
+      <h2 id="timer-label">{title}</h2>
+      <h4 id="time-left">{ minutes }:{ seconds }</h4>
+      <div className="control-buttons">
+        <button id="start_stop" onClick={handleStartStop}>{(breakRunning || sessionRunning) ? "Stop" : "Start"}</button>
+        <button id="reset" onClick={handleReset}>Reset</button>
+      </div>
+      <div className="setup-ctr">
+        <div className="session">
+          <h3 id="session-label">Session Length</h3>
+          <hr></hr>
+          <div className="btn-wrapper">
+            <button id="session-increment" onClick={() => handleInit("si")}>+</button>
+            <p id="session-length">{ sessionInit }</p>      
+            <button id="session-decrement" onClick={() => handleInit("sd")}>-</button> 
+          </div>
+        </div>
+        <div className="break">
+          <h3 id="break-label">Break Length</h3>
+          <hr></hr>
+          <div className="btn-wrapper">
+            <button id="break-increment" onClick={() => handleInit("bi")}>+</button>
+            <p id="break-length">{ breakInit }</p>
+            <button id="break-decrement" onClick={() => handleInit("bd")}>-</button>
+          </div>
+        </div>
+      </div>   
+      <div className="more-info">
+        <a id="website-link" href="https://www.jarenworme.com/">
+          <p>&copy;Jaren Worme 2024</p>
+        </a>
+        <a id="linkedin-link" href="https://www.linkedin.com/in/jarenworme/" target="_blank">
+          <i className="fab fa-linkedin"></i>
+        </a>
+        <a id="profile-link" href="https://github.com/jarenworme" target="_blank">
+          <i className="fab fa-github"></i>
+        </a>
+      </div>  
     </div>
   );
 }
